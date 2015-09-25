a926eaa63e20648a52c2516900dcbef0c754ef13
RealistExperimenter <n4ser@mail.ru>
2015-09-25 13:57:41 +0300

    make global refactoring and expanded console output

--- src/gelezka/RefreshGelezoTest.java
+++ src/gelezka/RefreshGelezoTest.java
@@ -4,7 +4,6 @@ package gelezka;
  */
 
 import org.junit.*;
-import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -13,55 +12,45 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.List;
 import java.util.Date;
+import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
 
 public class RefreshGelezoTest {
 
-
     private WebDriver driver;
-    private StringBuffer verificationErrors = new StringBuffer();
     private String userPassword, userName, baseUrl;
     private List<String> listOfLinks;
     private WebElement temp;
 
-    //  private final Wait<WebDriver> wait = new WebDriverWait(driver, 5).withMessage("Element was not found");
-
-    // @FindBy(id="password-shown")
-
-
-
-
-
-
 
     @Before
     public void setUp() throws Exception {
-        driver = new ChromeDriver();
-     //   driver = new FirefoxDriver();
         baseUrl = "http://gelezo.com.ua/";
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-
         ReadXML data = new ReadXML();
         userName=data.getUserName();
         userPassword=data.getUserPassword();
         listOfLinks=data.getListOfLinks();
     }
 
+    private void createDriver(){
+        driver = new ChromeDriver();
+        System.out.println("Site will be used with the user: "+userName);
+        //   driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
 
 
     @Test
     public void refresh(){
-        login();
         while(true) {
-
-        refreshAllPosts(listOfLinks);
-
+              createDriver();
+              login();
+              refreshAllPosts(listOfLinks);
+              driver.quit();
            try {
                Thread.sleep(7800 * 1000);
-           } catch (InterruptedException e) {
-           }
+           } catch (InterruptedException e) {System.out.println("Error under sleep");}
        }
-
     }
 
     private void login(){
@@ -80,9 +69,8 @@ public class RefreshGelezoTest {
         pass.sendKeys(userPassword);
         signIn.click();
 
-        if (driver.findElement(By.xpath("/html/body/div/table[1]/tbody/tr/td[2]/table/tbody/tr[1]/td[1]/a[1]/b")).getText().matches(userName)) {System.out.println("Site will be used with the user: "+userName);}
-        else  System.out.println("Can't login with the "+userName+" user"); System.out.println("");
-        //String s = (char)27 + "[36mbla-bla-bla"
+        if (!(driver.findElement(By.xpath("/html/body/div/table[1]/tbody/tr/td[2]/table/tbody/tr[1]/td[1]/a[1]/b")).getText().matches(userName))) {System.out.println("Can't login with the " + userName + " user");}
+         //String s = (char)27 + "[36mbla-bla-bla"
     }
 
     private void refreshAllPosts(List<String> listOfLinks){
@@ -90,6 +78,7 @@ public class RefreshGelezoTest {
         for (int i = 0; i < listOfLinks.size(); i++) {
             driver.get(listOfLinks.get(i));
             temp = driver.findElement(By.xpath("/html/body/div/table[1]/tbody/tr/td[2]/table/tbody/tr[2]/td[1]/table/tbody/tr/td[2]/a"));
+            System.out.println("");
             System.out.println("   Refreshing posts on \""+temp.getText()+"\" board");
             refreshPosts();
         }
@@ -101,19 +90,28 @@ public class RefreshGelezoTest {
 
     private void refreshPosts(){
 
-              List<WebElement> uname = driver.findElements(By.linkText("Обновить"));
+        List<WebElement> uname = driver.findElements(By.linkText("Обновить"));
+        int errCount=uname.size();
+
+        if (uname.size() == 0) {
+            System.out.println("!!!!!!There is no any post on the "+temp.getText()+"\" board. Please add some or delete a board link from the my_settings.xml file.");
+            return;
+        }
 
         for (int i = 0; i != uname.size(); i++) {
-            List<WebElement> names = driver.findElements(By.linkText("Обновить"));
-            names.get(i).click();
+          //  List<WebElement> names = driver.findElements(By.linkText("Обновить"));
+            uname.get(i).click();
             try {
-                Thread.sleep(2 * 1000);
-            } catch (InterruptedException e) {
-            }
+                Thread.sleep(1 * 1000);
+            } catch (InterruptedException e) {System.out.println("Error under sleep");}
+            try {
+                temp = driver.findElement(By.xpath("/html/body/div/table[3]/tbody/tr/td/font/h2"));
+            } catch (Exception e) {errCount--;  System.out.println("Some Error with err message element");}
             driver.navigate().back();
-
         }
-        System.out.println("     "+uname.size()+" posts successfully refreshed");
+
+        System.out.println("     "+(uname.size()-errCount)+" posts successfully refreshed");
+        if (errCount != 0)  System.out.println("     "+errCount+" posts not refreshed. Maybe available refresh time larger than 2 hours and 10 minutes.");
     }
 
 
